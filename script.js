a324de3f8bf91b7b4cf5871b267879243429f117
ifiplease <adegbiteanjolaoluwa15@gmail.com>
2023-08-06 22:49:33 +0100

    Add header and click-and-drag functionality for coloring grid divs

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ const numberOfDuplicates = 256;
 for (let i=0; i < numberOfDuplicates; i++) {
     const grids = document.createElement('div');
     container.appendChild(grids);
-    grids.classList.add('grids');
+    grids.classList.add('grids'); 
+    
+    grids.addEventListener('mousedown', () => {
+        grids.style.backgroundColor = 'black';
+    });
+
+    grids.addEventListener('mouseover', (event) => {
+        if (event.buttons === 1) {
+          grids.style.backgroundColor = 'black';
+        }
+    });
+
     
 }
