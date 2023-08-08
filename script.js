f66245bab30c1990ceee7d5f5d1e36089f0366e3
ifiplease <adegbiteanjolaoluwa15@gmail.com>
2023-08-08 01:09:54 +0100

    add functionality to color selection and rainbow buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
 const gridSize = 16;
 const buttonsContainer = document.querySelector('.buttons')
+const selectedColor = 'chosenColor';
 
 const numberOfDuplicates = gridSize * gridSize;
 for (let i=0; i < numberOfDuplicates; i++) {
@@ -9,12 +10,27 @@ for (let i=0; i < numberOfDuplicates; i++) {
     grids.classList.add('grids'); 
     
     grids.addEventListener('mousedown', () => {
-        grids.style.backgroundColor = 'black';
+        grids.style.backgroundColor = chosenColor;
     });
 
     grids.addEventListener('mouseover', (event) => {
         if (event.buttons === 1) {
-          grids.style.backgroundColor = 'black';
+            if (chosenColor === 'rainbow') {
+                const rainbowColors = [
+                    ['FF0000', 'FF3300', 'FF6600', 'FF9900', 'FFCC00', 'FFFF00'], // Red
+                    ['FF6600', 'FF8000', 'FF9900', 'FFB200', 'FFCC00', 'FFD900'], // Orange
+                    ['FFCC00', 'FFD100', 'FFD900', 'FFE000', 'FFEA00', 'FFFF00'], // Yellow
+                    ['00FF00', '33FF00', '66FF00', '99FF00', 'CCFF00', 'FFFF00'], // Green
+                    ['0000FF', '0019FF', '0033FF', '004CFF', '0066FF', '007FFF'], // Blue
+                    ['4B0082', '4C1982', '4D3282', '4E4C82', '4F6582', '507F82'], // Indigo
+                    ['9400D3', '9500C0', '9600AD', '970097', '980084', '990071'] // Violet
+                ];
+                const colorGroup = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+                const randomColor = colorGroup[Math.floor(Math.random() * colorGroup.length)]
+                grids.style.backgroundColor = `#${randomColor}`;
+            } else {
+                grids.style.backgroundColor = chosenColor;
+            }        
         }
     });
 }
@@ -38,3 +54,19 @@ buttonConfigs.forEach(buttonConfig => {
         handleButtonClick(buttonConfig.action, buttonConfig.params);
     });
 });
+
+function handleButtonClick (action, params) {
+    switch (action) {
+        case 'colorSelection': 
+            const colorPicker = document.createElement('input');
+            colorPicker.type = 'color';
+            colorPicker.addEventListener('input', (event) => {
+                chosenColor = event.target.value;
+            });
+            colorPicker.click();
+        break;
+        case 'rainbow':
+            chosenColor = 'rainbow';
+        break;   
+    }
+}
