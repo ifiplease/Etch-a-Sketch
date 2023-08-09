973593b062d678b0bf72c0d1d6ea49ff964c8521
ifiplease <adegbiteanjolaoluwa15@gmail.com>
2023-08-09 19:56:22 +0100

    add functionality to changeGridSize, eraser and clear buttons
    create button elements for gridSize and remove dynamically created gridsize buttons
    
    use display grid for container instead of flex

--- script.js
+++ script.js
@@ -1,47 +1,60 @@
 const container = document.querySelector('.container');
-const gridSize = 16;
 const buttonsContainer = document.querySelector('.buttons')
-const selectedColor = 'chosenColor';
+let gridSize = 16;
+let chosenColor = '#000000';
 
-const numberOfDuplicates = gridSize * gridSize;
-for (let i=0; i < numberOfDuplicates; i++) {
-    const grids = document.createElement('div');
-    container.appendChild(grids);
-    grids.classList.add('grids'); 
-    
-    grids.addEventListener('mousedown', () => {
-        grids.style.backgroundColor = chosenColor;
-    });
+function updateGridSize (size){
+    container.innerHTML = "";
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    grids.addEventListener('mouseover', (event) => {
-        if (event.buttons === 1) {
-            if (chosenColor === 'rainbow') {
-                const rainbowColors = [
-                    ['FF0000', 'FF3300', 'FF6600', 'FF9900', 'FFCC00', 'FFFF00'], // Red
-                    ['FF6600', 'FF8000', 'FF9900', 'FFB200', 'FFCC00', 'FFD900'], // Orange
-                    ['FFCC00', 'FFD100', 'FFD900', 'FFE000', 'FFEA00', 'FFFF00'], // Yellow
-                    ['00FF00', '33FF00', '66FF00', '99FF00', 'CCFF00', 'FFFF00'], // Green
-                    ['0000FF', '0019FF', '0033FF', '004CFF', '0066FF', '007FFF'], // Blue
-                    ['4B0082', '4C1982', '4D3282', '4E4C82', '4F6582', '507F82'], // Indigo
-                    ['9400D3', '9500C0', '9600AD', '970097', '980084', '990071'] // Violet
-                ];
-                const colorGroup = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
-                const randomColor = colorGroup[Math.floor(Math.random() * colorGroup.length)]
-                grids.style.backgroundColor = `#${randomColor}`;
-            } else {
-                grids.style.backgroundColor = chosenColor;
-            }        
-        }
-    });
+    for (let i=0; i < size * size; i++) {
+        const gridCell = document.createElement('div');
+        container.appendChild(gridCell);
+        gridCell.classList.add('cell');
+        
+        
+        container.addEventListener('mousedown', (event) => {
+            const gridCell = event.target;
+            if(gridCell.classList.contains('cell')){
+                gridCell.style.backgroundColor = chosenColor;
+            }
+        });
+
+        container.addEventListener('mouseover', (event) => {
+            if (event.buttons === 1) {
+                const gridCell = event.target;
+                if (gridCell.classList.contains('cell')){
+                    if (chosenColor === 'rainbow') {
+                        const rainbowColors = [
+                            ['FF0000', 'FF3300', 'FF6600', 'FF9900', 'FFCC00', 'FFFF00'], // Red
+                            ['FF6600', 'FF8000', 'FF9900', 'FFB200', 'FFCC00', 'FFD900'], // Orange
+                            ['FFCC00', 'FFD100', 'FFD900', 'FFE000', 'FFEA00', 'FFFF00'], // Yellow
+                            ['00FF00', '33FF00', '66FF00', '99FF00', 'CCFF00', 'FFFF00'], // Green
+                            ['0000FF', '0019FF', '0033FF', '004CFF', '0066FF', '007FFF'], // Blue
+                            ['4B0082', '4C1982', '4D3282', '4E4C82', '4F6582', '507F82'], // Indigo
+                            ['9400D3', '9500C0', '9600AD', '970097', '980084', '990071'] // Violet
+                        ];
+                        const colorGroup = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+                        const randomColor = colorGroup[Math.floor(Math.random() * colorGroup.length)]
+                        gridCell.style.backgroundColor = `#${randomColor}`;
+                    } else {
+                        gridCell.style.backgroundColor = chosenColor;
+                    }
+                }        
+            }
+        });
+    }
+}
+
+function changeGridSize (size) {
+    gridSize = size;
+    updateGridSize(gridSize);
 }
 
 const buttonConfigs = [
     {name: 'Color Selection', action: 'colorSelection'},
     { name: 'Rainbow', action: 'rainbow' },
-    { name: 'Size: 16', action: 'gridSize', params: [16] },
-    { name: 'Size: 32', action: 'gridSize', params: [32] },
-    { name: 'Size: 64', action: 'gridSize', params: [64] },
-    { name: 'Size: 100', action: 'gridSize', params: [100] },
     { name: 'Eraser', action: 'eraser' },
     { name: 'Clear', action: 'clear' } 
 ];
@@ -51,11 +64,18 @@ buttonConfigs.forEach(buttonConfig => {
     button.textContent = buttonConfig.name;
     buttonsContainer.appendChild(button);
     button.addEventListener('click', () => {
-        handleButtonClick(buttonConfig.action, buttonConfig.params);
+        handleButtonClick(buttonConfig.action);
     });
 });
 
-function handleButtonClick (action, params) {
+function clearGrid() {
+    const gridCells = document.querySelectorAll('.cell')
+    gridCells.forEach(gridCell => {
+        gridCell.style.backgroundColor = '';
+    })
+}
+
+function handleButtonClick (action) {
     switch (action) {
         case 'colorSelection': 
             const colorPicker = document.createElement('input');
@@ -67,6 +87,14 @@ function handleButtonClick (action, params) {
         break;
         case 'rainbow':
             chosenColor = 'rainbow';
-        break;   
+        break;
+        case 'eraser':
+            chosenColor = 'white';
+        break;
+        case 'clear':
+            clearGrid();
+        break;
     }
 }
+
+updateGridSize(gridSize);
